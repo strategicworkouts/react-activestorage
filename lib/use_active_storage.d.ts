dfd785a4fa3a6850a6d97eff8241f0c91a9a768f
Strategic Workouts <strategicworkouts@gmail.com>
2024-09-04 22:10:42 -0700

    build headers

--- lib/use_active_storage.d.ts
+++ lib/use_active_storage.d.ts
@@ -6,12 +6,14 @@ type Blob = {
     signed_id: string;
     filename: string;
 };
+interface HashMap<T> {
+    [key: string]: T;
+}
 type Callback = (params: {
     blob?: Blob;
     error?: Error;
 }) => void;
-type CSRF = string | null | (() => Promise<string>);
-export declare const useActiveStorage: (file?: File, callback?: Callback, csrf?: CSRF, URL?: string) => {
+export declare const useActiveStorage: (file?: File, callback?: Callback, storageHeaders?: HashMap<string>, URL?: string) => {
     uploading: boolean;
     progress?: Progress;
 };
